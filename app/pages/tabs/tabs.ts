65f1f678c21ec6f6e5ff77712029b6406efd86b2
Darek Dziamalek <ddziamalek@gmail.com>
2016-07-26 02:54:54 -0400

    its 3am

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
 import {AboutPage} from '../about/about';
 import {ContactPage} from '../contact/contact';
+import {WorkoutsPage} from "../workouts/workouts";
+import {AddWorkoutPage} from "../add-workout/add-workout";
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -11,6 +13,8 @@ export class TabsPage {
   private tab1Root: any;
   private tab2Root: any;
   private tab3Root: any;
+  private workoutsRoot: any;
+  private addWorkoutRoot: any;
 
   constructor() {
     // this tells the tabs component which Pages
@@ -18,5 +22,7 @@ export class TabsPage {
     this.tab1Root = HomePage;
     this.tab2Root = AboutPage;
     this.tab3Root = ContactPage;
+    this.workoutsRoot = WorkoutsPage;
+    this.addWorkoutRoot = AddWorkoutPage;
   }
 }
