0112a7ce5d8c1efd0116493db598e2bec2a62f01
Darek Dziamalek <ddziamalek@gmail.com>
2016-07-26 22:45:33 -0400

    search started

--- app/pages/search/search.ts
+++ app/pages/search/search.ts
@@ -1,18 +1,150 @@
 import {Component, OnInit} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, ActionSheet, Platform, Alert, Loading} from 'ionic-angular';
 import {GiphyService} from "../../services/giphy.service";
 
 @Component({
   providers: [GiphyService],
-  templateUrl: 'build/pages/giphy/giphy.html'
+  templateUrl: 'build/pages/search/search.html'
 })
 export class SearchPage implements OnInit {
-  gifs=[];
+  gifs = [];
+  ratingRadioOpen:boolean;
+  ratingRadioResult = 'any';
+  query:String;
+  isLoading:boolean = false;
+  loading:Loading;
 
   ngOnInit() {
-    this._giphyService.getGifBySearch().subscribe(gifs=>this.gifs = gifs);
+    // this._giphyService.getGifBySearch().subscribe(gifs=>this.gifs = gifs);
   }
 
-  constructor(private navCtrl:NavController, private _giphyService:GiphyService) {
+  constructor(private navCtrl:NavController, public platform:Platform, private _giphyService:GiphyService) {
+  }
+
+  doSearch(event) {
+    this.query = event.target.value;
+    if (this.query && this.query != '') {
+      this.toggleLoading();
+      this._giphyService.getGifBySearch(this.query, this.ratingRadioResult).subscribe(gifs=> {
+        this.gifs = gifs.data;
+        console.log(gifs);
+        this.toggleLoading();
+      });
+    }
+  }
+
+  doRating() {
+    let alert = Alert.create();
+    alert.setTitle('Filter Rating');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'any',
+      value: 'any',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Kids',
+      value: 'y'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'G',
+      value: 'g'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'PG',
+      value: 'pg'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'PG-13',
+      value: 'pg-13'
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'R',
+      value: 'r'
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Ok',
+      handler: data => {
+        console.log('Radio data:', data);
+        this.ratingRadioOpen = false;
+        this.ratingRadioResult = data;
+      }
+    });
+
+    this.navCtrl.present(alert).then(() => {
+      this.ratingRadioOpen = true;
+    });
+  }
+
+  openMenu() {
+    let actionSheet = ActionSheet.create({
+      title: "Stick Yo' GIF",
+      cssClass: 'search',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    this.navCtrl.present(actionSheet);
+  }
+
+  toggleFavorite(gif) {
+
+  }
+
+  copyToClipboard(gif) {
+
+  }
+
+  shareGif(gif) {
+
+  }
+
+  // loading() {
+  //   let loading = Loading.create({
+  //     content: "GIF Grinding...",
+  //     dismissOnPageChange: true
+  //   });
+  // }
+
+  toggleLoading() {
+    if (!this.isLoading) {
+      this.loading = Loading.create({
+        content: "GIF Grinding...",
+        dismissOnPageChange: true
+      });
+      this.navCtrl.present(this.loading);
+    }
+    else this.loading.dismiss();
+    this.isLoading = !this.isLoading;
   }
 }
--- app/services/giphy.service.ts
+++ app/services/giphy.service.ts
@@ -5,17 +5,23 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class GiphyService {
-  apiKey:String;
+  api_key:String;
   giphyUrl:String;
+  hasRating:boolean;
 
   constructor(private _http:Http) {
     console.log('Service Connected...');
-    this.apiKey = 'DHXPgKmwwyhBNdSFQFKqIc4fEktFGSG3';
-    this.giphyUrl = 'https://api.mlab.com/api/1/databases/fauionic2/collections/workouts';
+    this.api_key = 'dc6zaTOxFJmzC';
+    this.giphyUrl = 'http://api.giphy.com/v1/gifs';
   }
 
-  getGifBySearch():Observable {
-    return this._http.get(`${this.giphyUrl}?apiKey=${this.apiKey}`).map(res=>res.json());
+  getGifBySearch(query:String, rating:String):Observable<any> {
+    let parameters = 'q='+query.replace(' ', '+');
+    this.hasRating = rating == 'any' ? false : true;
+    if(this.hasRating){
+      parameters += '&r='+rating;
+    }
+    return this._http.get(`${this.giphyUrl}/search?${parameters}&api_key=${this.api_key}`).map(res=>res.json());
   }
 
 }
